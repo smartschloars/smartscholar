8b8203b2158c4f8a858988c755166e21f2aeebaf
Srinu Duggempudi <duggempudisrinu520@gmail.com>
2025-10-25 03:31:06 +0530

    updated gor gh pages

--- next.config.js
+++ next.config.js
@@ -6,4 +6,9 @@ const nextConfig = {
   },
 }
 
+// For GitHub Pages deployment (static export)
+if (process.env.DEPLOY_TARGET === 'github-pages') {
+  nextConfig.output = 'export'
+}
+
 module.exports = nextConfig
